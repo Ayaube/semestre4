1f6fd6589c68d66eb12bcc015b28ff83ba17e78f
Ayaube <ayoub.lamchichi.info@gmail.com>
2024-03-19 15:09:10 +0100

    ricordeau

--- Optimisation/TP1/2.py
+++ Optimisation/TP1/2.py
@@ -4,40 +4,28 @@ import matplotlib; matplotlib.use('TkAgg')
 
 plt.close('all')
 
-
 def fonction(x):
-    return x*x
-
+    return x * x
 
 def derfonction(x):
-    return 2*x
+    return 2 * x
 
-
-# Tracer la fonction F(x) :
 xmin = -5
 xmax = 5
 plt.figure()
 x = np.linspace(xmin, xmax, 150)
 plt.plot(x, fonction(x), 'b-', linewidth=1.5)
 
-# Paramètres de l'algorithme :
 alpha = 0.2
 xinit = 4
 xsuite = [xinit]
 kmax = 15
 
-
-
 for k in range(kmax):
     plt.plot(xsuite[k], fonction(xsuite[k]), 'ro', alpha=0.5)
-    plt.pause(0.8)  # Pause avec durée en secondes
+    plt.pause(0.8)
 
-    # Calcul de la prochaine valeur :
     slope = derfonction(xsuite[k])
     xsuite.append(xsuite[k] - alpha * slope)
-
-
-
-
 
 plt.show()
--- Optimisation/TP1/gradient_2var_3D_ETUD.py
+++ Optimisation/TP1/gradient_2var_3D_ETUD.py
@@ -1,64 +1,41 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 17 14:41:21 2023
-
-@author: ricord
-"""
-
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 plt.close('all')
 
-
 X = np.arange(-5, 5, 0.25)
 Y = np.arange(-5, 5, 0.25)
-
-# Grille de lissage faite pour toutes les valeurs de X et Y
-
 X, Y = np.meshgrid(X, Y)
-
-Z = X*X + Y*Y
-
-# Affichage de la nappe
+Z = X**2 + Y**2
 
 fig = plt.figure()
-ax = fig.gca(projection='3d')
+ax = fig.add_subplot(111, projection='3d')
+
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
-
-surface = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap="plasma",
-                          linewidth=0, antialiased=False, alpha = 0.5)
+surface = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap="plasma", linewidth=0, antialiased=False, alpha=0.5)
 fig.colorbar(surface, shrink=0.5, aspect=5)
-
 
 def fonction(a, b):
     return a**2 + b**2
 
-
-# le vecteur gradient :
 def gradient(a, b):
-    grad = np.array( ?????? )
+    grad = np.array([2*a, 2*b])
     return grad
 
-
 alpha = 0.1
-
 ainit = -3
 binit = 3
-paraminit = np.array([[ainit],
-                      [binit]])
+paraminit = np.array([ainit, binit])
 
 kmax = 15
 param = paraminit
 
 for k in range(kmax):
-    a = param[0, 0]
-    b = param[1, 0]
-    ax.scatter(a, b, fonction(a, b), c='black', s=40)  # s=40 #taille
-    # nouvelles valeurs pour a et b :
-    param = ???????
+    grad = gradient(param[0], param[1])
+    param = param - alpha * grad
+    ax.scatter(param[0], param[1], fonction(param[0], param[1]), c='black', s=40)
 
 ax.scatter(ainit, binit, fonction(ainit, binit), c='red', s=40)
 
